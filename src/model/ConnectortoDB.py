241ff1c198cce436773a6dcee6c6ee63bfca1af4
wgeis <sa.wge@specialisterne.com>
2022-09-28 14:53:06 +0200

    WordselectionAndValidationComplete

--- src/model/ConnectortoDB.py
+++ src/model/ConnectortoDB.py
@@ -3,14 +3,23 @@ import random
 #attributes and lists
 source = "src\model\possible_words.txt"
 used= "src\model\\used_words.txt"
+#find word:
 
+#single line:
+#randomword = random.choice(findWords(source))
+
+#print(randomWord)
+#compare to list:
+
+
+#merged code:
 
 
 def checkIfWordIsUsedInFile(txtfile,word):
     wordUsed=False    
     with  open(txtfile,"r") as file:
         
-        lines=file.read()
+        lines=file.readlines()
         if(word in lines):
             wordUsed=True
 
@@ -18,22 +27,36 @@ def checkIfWordIsUsedInFile(txtfile,word):
     
 
 
-print(check)
-
-
 def findWords(txtfile):
-    file = open(txtfile,"r")
-    lines=file.readlines()
-    file.close()
+
+    with  open(txtfile,"r") as file:
+        
+        lines=file.readlines()
+    
+    
+    
     return lines
 
-wordlist = findWords(source)
 
 
-#find word:
-randomWord=random.choice(wordlist)
-check =checkIfWordIsUsedInFile(used,randomWord)
-#compare to list:
+def addWordIfNotExists(txtfile,word):
+    if not(checkIfWordIsUsedInFile(txtfile=txtfile,word=word)):
+        
+        with  open(txtfile,"a") as file:
+            file.write(word)
+        print("word added:"+word)
+
+
+
+def searchAndCompareRandomWordToGuess():
+    wordlist = findWords(source)
+    randomWord=random.choice(wordlist)
+    print(checkIfWordIsUsedInFile(used,randomWord))
+    #Add if not exists
+    addWordIfNotExists(used,randomWord)
+
+searchAndCompareRandomWordToGuess()
+
 
 
 
